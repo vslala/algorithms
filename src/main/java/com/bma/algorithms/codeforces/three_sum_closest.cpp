a9e48015d89eeb4423e7fa17e75b8082cab5553c
Varun shrivastava <varunshrivastava007@gmail.com>
2020-11-03 23:37:07 +0530

    solve 3sum closest leetcode problem

--- src/main/java/com/bma/algorithms/codeforces/three_sum_closest.cpp
+++ src/main/java/com/bma/algorithms/codeforces/three_sum_closest.cpp
@@ -8,18 +8,16 @@ using namespace std;
 
 #define sum(nums, i, l, r) (nums[i] + nums[l] + nums[r])
 
-vector<vector<int>> threeSum(vector<int>& nums) {
+int threeSum(vector<int>& nums, int target) {
     if (nums.empty()) return {};
 
     sort(nums.begin(), nums.end());
-    int i = 0, l, r = nums.size() - 1;
-
-    vector<vector<int>> output;
+    int i = 0, l = i + 1, r = nums.size() - 1, output = 0, diff = INT_MAX;
 
     while (i < nums.size() - 2) {
         l = i + 1;
         r = nums.size() - 1;
-        
+
         // skip the loop because the triplet have already been counted for the same value of the i
         if (i > 0 && nums[i] == nums[i - 1]) {
             i ++;
@@ -27,18 +25,24 @@ vector<vector<int>> threeSum(vector<int>& nums) {
         }
 
         while (l < r) {
-            cout << i << ":" << l << ":" << r << ", ";
+            // cout << i << ":" << l << ":" << r << ", ";
             if (l > i + 1 && nums[l] == nums[l - 1]) {
                 l++;
                 continue;
             }
+            
+            // calc smallest diff
+            int curr_diff = abs(target - sum(nums, i, l, r));
+            if (diff > curr_diff) {
+                output = sum(nums, i, l, r);
+                diff = curr_diff;
+            }
 
-            if (sum(nums, i, l, r)  > 0) r--;
-            else if (sum(nums, i, l, r) < 0) l++;
+            if (sum(nums, i, l, r) > target) r--;
+            else if (sum(nums, i, l, r) < target) l++;
             else {
-                output.push_back({nums[i], nums[l], nums[r]});
-                l++;
-                r--;
+                output = sum(nums,i, l, r);
+                return output;
             }
         }
 
@@ -50,17 +54,11 @@ vector<vector<int>> threeSum(vector<int>& nums) {
 }
 
 int main() {
-    vector<int> nums = {-1,0,1,2,-1,-4};
+    vector<int> nums = {-1,2,1,-4};
+    int target = 1;
 
-    vector<vector<int>> output = threeSum(nums); 
+    int output = threeSum(nums, target); 
 
     cout << "\n";
-    for (auto itr = output.begin(); itr != output.end(); itr++) {
-        vector<int> sub_set = *itr;
-        cout << "[";
-        for (int i = 0; i < sub_set.size(); i++) {
-            cout << sub_set[i] << ",";
-        }
-        cout << "]";
-    }
+    cout << "SUM: " << output;
 }
