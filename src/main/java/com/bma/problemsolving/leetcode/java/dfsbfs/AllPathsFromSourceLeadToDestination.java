7238f35f70bd4704cb19f087af7b5e681a72f242
Varun Shrivastava <varun.shrivastava@klarna.com>
2021-12-26 21:57:19 +0100

    restructure the code to be extended with BFS later

--- src/main/java/com/bma/problemsolving/leetcode/java/dfsbfs/AllPathsFromSourceLeadToDestination.java
+++ src/main/java/com/bma/problemsolving/leetcode/java/dfsbfs/AllPathsFromSourceLeadToDestination.java
@@ -25,45 +25,60 @@ class AllPathsFromSourceLeadToDestination {
         GRAY,
         BLACK
     }
-    private int destination;
-    private Color[] state;
-    private Map<Integer, List<Integer>> graph;
 
     public boolean leadsToDestination(int n, int[][] edges, int source, int destination) {
-        this.destination = destination;
-        this.state = new Color[n];
-        this.graph = new HashMap<>();
-
-        Arrays.fill(state, Color.WHITE);
-        IntStream.range(0, n).forEach(vertex -> graph.put(vertex, new LinkedList<>()));
-        IntStream.range(0, edges.length).forEach(index -> graph.get(edges[index][0]).add(edges[index][1]));
-
-        return doesItLeadsToDestination(source);
+        var dfsResult = new DFS(n, edges).doesItLeadsToDestination(source, destination);
+        return dfsResult;
     }
 
-    private boolean doesItLeadsToDestination(int currVertex) {
-        // if state is gray, then it creates a backward edge, therefore it is a loop
-        if (state[currVertex] != Color.WHITE) {
-            return state[currVertex] == Color.BLACK;
+    interface SourceToDestinationPathFinder {
+        boolean doesItLeadsToDestination(int start, int destination);
+        default Map<Integer, List<Integer>> buildDiGraph(int vertices, int[][] edges) {
+            Map<Integer, List<Integer>> graph = new HashMap<>();
+            IntStream.range(0, vertices).forEach(vertex -> graph.put(vertex, new LinkedList<>()));
+            IntStream.range(0, edges.length).forEach(index -> graph.get(edges[index][0]).add(edges[index][1]));
+
+            return graph;
+        }
+    }
+
+    private static class DFS implements SourceToDestinationPathFinder {
+
+        private AllPathsFromSourceLeadToDestination.Color[] state;
+        private Map<Integer, List<Integer>> graph;
+
+        public DFS(int vertices, int[][] edges) {
+            this.state = new AllPathsFromSourceLeadToDestination.Color[vertices];
+            this.graph = buildDiGraph(vertices, edges);
+
+            Arrays.fill(state, AllPathsFromSourceLeadToDestination.Color.WHITE);
         }
 
-        // if destination node is the leaf node then return true
-        if (graph.get(currVertex).isEmpty()) {
-            return currVertex == destination;
+        @Override
+        public boolean doesItLeadsToDestination(int currVertex, int destination) {
+            // if state is gray, then it creates a backward edge, therefore it is a loop
+            if (state[currVertex] != AllPathsFromSourceLeadToDestination.Color.WHITE) {
+                return state[currVertex] == AllPathsFromSourceLeadToDestination.Color.BLACK;
+            }
+
+            // if destination node is the leaf node then return true
+            if (graph.get(currVertex).isEmpty()) {
+                return currVertex == destination;
+            }
+
+            // processing this node, so marking it as GRAY
+            state[currVertex] = AllPathsFromSourceLeadToDestination.Color.GRAY;
+
+            for (int u : graph.get(currVertex)) {
+                // if we got `false` anywhere down the road, we short-circuit and return false
+                if (!doesItLeadsToDestination(u, destination))
+                    return false;
+            }
+            // recursive processing done for the node, so marking it as BLACK
+            state[currVertex] = AllPathsFromSourceLeadToDestination.Color.BLACK;
+
+            // if no negative stage found, return true
+            return true;
         }
-
-        // processing this node, so marking it as GRAY
-        state[currVertex] = Color.GRAY;
-
-        for (int u : graph.get(currVertex)) {
-            // if we got `false` anywhere down the road, we short-circuit and return false
-            if (!doesItLeadsToDestination(u))
-                return false;
-        }
-        // recursive processing done for the node, so marking it as BLACK
-        state[currVertex] = Color.BLACK;
-
-        // if no negative stage found, return true
-        return true;
     }
 }
