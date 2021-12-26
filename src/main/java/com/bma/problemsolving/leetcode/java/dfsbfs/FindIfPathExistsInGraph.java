491502fd9c8fbf605af11b8eaf4c79af46dfc508
Varun Shrivastava <varun.shrivastava@klarna.com>
2021-12-26 15:01:04 +0100

    add BFS solution to the same problem | DFS, BFS, Graph

--- src/main/java/com/bma/problemsolving/leetcode/java/dfsbfs/FindIfPathExistsInGraph.java
+++ src/main/java/com/bma/problemsolving/leetcode/java/dfsbfs/FindIfPathExistsInGraph.java
@@ -14,30 +14,93 @@ import java.util.stream.IntStream;
 class FindIfPathExistsInGraph {
 
     public boolean validPath(int vertices, int[][] edges, int start, int end) {
-        if (vertices == 1) return true;
+        var dfsResult = new DFS(vertices, edges).validPath(start, end);
+        var bfsResult = new BFS(vertices, edges).validPath(start, end);
+        assert dfsResult == bfsResult;
 
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        IntStream.range(0, vertices).forEach(v -> graph.put(v, new LinkedList<>()));
-        IntStream.range(0, edges.length).forEach(v -> {
-            graph.get(edges[v][0]).add(edges[v][1]);
-            graph.get(edges[v][1]).add(edges[v][0]);
-        });
+        return dfsResult;
+    }
 
-        boolean[] visited = new boolean[vertices];
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-        stack.push(start);
-        while (!stack.isEmpty()) {
-            int v = stack.pop();
-            if (!visited[v]) {
-                visited[v] = true;
-                for(int u: graph.get(v)) {
-                    if (u == end) return true;
-                    if (!visited[u])
-                        stack.push(u);
-                }
-            }
+    interface ValidPath {
+        boolean validPath(int start, int end);
+
+        default Map<Integer, List<Integer>> buildDiGraph(int vertices, int[][] edges) {
+            Map<Integer, List<Integer>> graph = new HashMap<>();
+
+            IntStream.range(0, vertices)
+                    .forEach(v -> graph.put(v, new LinkedList<>()));
+
+            IntStream.range(0, edges.length).forEach(v -> {
+                graph.get(edges[v][0]).add(edges[v][1]);
+                graph.get(edges[v][1]).add(edges[v][0]);
+            });
+
+            return graph;
+        }
+    }
+
+    private static class DFS implements ValidPath {
+        private Map<Integer, List<Integer>> graph;
+        private boolean[] visited;
+        private int vertices;
+
+        public DFS(int vertices, int[][] edges) {
+            this.vertices = vertices;
+            this.graph = buildDiGraph(vertices, edges);
+            this.visited = new boolean[vertices];
         }
 
-        return false;
+        @Override
+        public boolean validPath(int start, int end) {
+            if (this.vertices == 1) return true;
+
+            ArrayDeque<Integer> stack = new ArrayDeque<>();
+            stack.push(start);
+            while (!stack.isEmpty()) {
+                int v = stack.pop();
+                if (!visited[v]) {
+                    visited[v] = true;
+                    for (int u : graph.get(v)) {
+                        if (u == end) return true;
+                        if (!visited[u])
+                            stack.push(u);
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+
+    private static class BFS implements ValidPath {
+        private Map<Integer, List<Integer>> graph;
+        private int vertices;
+        private boolean[] visited;
+
+        public BFS(int totalVertices, int[][] edges) {
+            this.vertices = totalVertices;
+            this.graph = buildDiGraph(totalVertices, edges);
+            this.visited = new boolean[totalVertices];
+        }
+
+        @Override
+        public boolean validPath(int start, int end) {
+            if (this.vertices == 1) return true;
+            Deque<Integer> queue = new ArrayDeque<>();
+            queue.offer(start);
+            while (!queue.isEmpty()) {
+                int v = queue.poll();
+                for (int u: graph.get(v)) {
+                    if (!visited[u]) {
+                        if (u == end)
+                            return true;
+                        queue.offer(u);
+                    }
+                }
+                visited[v] = true;
+            }
+
+            return false;
+        }
     }
 }
