05995d2d0e34c3b8978005a8e16f94e0fd76c3f2
Varun Shrivastava <varun.shrivastava@klarna.com>
2021-12-29 18:28:43 +0100

    revisit Prim's Lazy algorithm and do some refactoring

--- src/main/java/com/bma/algorithms/graphs/minimum_spanning_trees/PrimsAlgorithmLazy.java
+++ src/main/java/com/bma/algorithms/graphs/minimum_spanning_trees/PrimsAlgorithmLazy.java
@@ -3,13 +3,26 @@ package com.bma.algorithms.graphs.minimum_spanning_trees;
 import com.bma.algorithms.graphs.model.Edge;
 import com.bma.algorithms.priorityqueues.MinHeap;
 
+/**
+ * Prim's Lazy Algorithm for computing Minimum Spanning Tree
+ * ----------------------------------------------------------
+ * Another algorithm for finding the minimum spanning tree from a griven graph.
+ * This is a lazy algorithm in a sense that the next min-edge is computed lazily.
+ * The idea is pretty simple-
+ * 1. Visit all the incident edges from the vertex v and add them to priority queue (min heap)
+ * 2. Delete the min-edge from the priority queue and check if it has already been processed or marked
+ * 3. If the edge has not been processed before than add it to the MST
+ * 4. Repeat the process until the queue is empty or total edge count reaches to V - 1.
+ *
+ * @author varun.shrivastava
+ */
 class PrimsAlgorithmLazy {
 
     private boolean[] marked;
     private MinHeap<Edge> pq = new MinHeap<>();
     private EdgeWeightedGraph mst;
 
-    public EdgeWeightedGraph mst(EdgeWeightedGraph graph, int sourceVertex) {
+    public PrimsAlgorithmLazy(EdgeWeightedGraph graph, int sourceVertex) {
         marked = new boolean[graph.totalVertices()];
         mst = new EdgeWeightedGraphImpl(graph.totalVertices());
 
@@ -26,7 +39,13 @@ class PrimsAlgorithmLazy {
             if (!marked[v]) visit(graph, v);
             if (!marked[w]) visit(graph, w);
         }
+    }
 
+    public PrimsAlgorithmLazy(EdgeWeightedGraph graph) {
+        this(graph, 0);
+    }
+
+    public EdgeWeightedGraph minimumSpanningTree() {
         return mst;
     }
 
@@ -36,5 +55,14 @@ class PrimsAlgorithmLazy {
             if (!marked[edge.other(sourceVertex)])
                 pq.add(edge);
         }
+    }
+
+    public double minWeightOfSpanningTree() {
+        double minWeight = 0.0d;
+        for (Edge edge : mst.edges()) {
+            minWeight += edge.getWeight();
+        }
+
+        return minWeight;
     }
 }
--- src/test/java/com/bma/algorithms/graphs/minimum_spanning_trees/PrimsAlgorithmLazyTest.java
+++ src/test/java/com/bma/algorithms/graphs/minimum_spanning_trees/PrimsAlgorithmLazyTest.java
@@ -1,9 +1,11 @@
 package com.bma.algorithms.graphs.minimum_spanning_trees;
 
-import com.bma.algorithms.graphs.model.Edge;
-import com.bma.algorithms.sort.elementary.Util;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PrimsAlgorithmLazyTest {
 
@@ -12,21 +14,13 @@ class PrimsAlgorithmLazyTest {
 
     @BeforeEach
     void setup() {
-        graph = new EdgeWeightedGraphImpl(10);
-        graph.addEdge(new Edge(0, 2, 0.25));
-        graph.addEdge(new Edge(2, 1, 0.15));
-        graph.addEdge(new Edge(1, 3, 0.35));
-        graph.addEdge(new Edge(3, 5, 0.55));
-        graph.addEdge(new Edge(5, 7, 0.75));
-        graph.addEdge(new Edge(7, 9, 0.45));
-
-        prims = new PrimsAlgorithmLazy();
+        graph = new EdgeWeightedGraphImpl(Path.of("src/main/resources/algorithms/graphs/minimum_spanning_trees/test_graph.csv"));
+        prims = new PrimsAlgorithmLazy(graph);
     }
 
     @Test
     void itShouldCreateMinimumSpanningTree() {
-        graph.addEdge(new Edge(9, 0, 0.15));
-        EdgeWeightedGraph mst = prims.mst(graph, 0);
-        mst.edges().forEach(Util::println);
+        double minWeight = prims.minWeightOfSpanningTree();
+        assertEquals(1.81, minWeight);
     }
 }
