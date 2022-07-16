965af960e02a71ed60d37c64c6bbaf142daf7300
Varun Shrivastava <varun.shrivastava@klarna.com>
2022-07-16 12:21:00 +0200

    implement Simple Leader Election algorithm with Zookeeper

---
+++ src/main/java/com/bma/systemdesign/leaderelection/LeaderElection.java
@@ -0,0 +1,84 @@
+package com.bma.systemdesign.leaderelection;
+
+import lombok.SneakyThrows;
+import org.apache.zookeeper.*;
+
+import java.util.Collections;
+import java.util.List;
+
+class LeaderElection implements Watcher {
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 3000;
+    private static final String ELECTION_NAMESPACE = "/election";
+
+    private ZooKeeper zooKeeper;
+    private String currentZnodeName;
+
+    @SneakyThrows
+    void connectToZookeeper() {
+        this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+        this.volunteerForElection();
+        this.electLeader();
+        this.run();
+        System.out.println("Disconnected from Zookeeper. Exiting application.");
+    }
+
+    /**
+     * will be called by zookeeper library on a separate thread whenever there is an event
+     * @param event
+     */
+    @Override
+    public void process(WatchedEvent event) {
+        switch (event.getType()) {
+            case None:
+                if (event.getState() == Event.KeeperState.SyncConnected) {
+                    System.out.println("Successfully connected to Zookeeper event.");
+                } else {
+                    synchronized (zooKeeper) {
+                        System.out.println("Disconnected from Zookeeper event");
+                        zooKeeper.notifyAll();
+                    }
+                }
+        }
+    }
+
+    @SneakyThrows
+    public void run() {
+        synchronized (zooKeeper) {
+            zooKeeper.wait();
+        }
+    }
+
+    public static void main(String[] args) {
+        LeaderElection leaderElection = new LeaderElection();
+        leaderElection.connectToZookeeper();
+    }
+
+    @SneakyThrows
+    public void close() {
+        zooKeeper.close();
+    }
+
+    @SneakyThrows
+    public void volunteerForElection() {
+        String zNodePrefix = ELECTION_NAMESPACE + "/c_";
+        String zNodeFullPath = this.zooKeeper.create(zNodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        System.out.println("Znode Full Path: " + zNodeFullPath);
+
+        this.currentZnodeName = zNodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+    }
+
+    @SneakyThrows
+    public void electLeader() {
+        List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+        Collections.sort(children);
+        String smallestChild = children.get(0);
+
+        if (smallestChild.equalsIgnoreCase(currentZnodeName)) {
+            System.out.println(currentZnodeName + ": I'm the leader.");
+        } else {
+            System.out.println(currentZnodeName + ": I'm not the leader.");
+        }
+    }
+}
---
+++ src/test/java/com/bma/systemdesign/leaderelection/LeaderElectionTest.java
@@ -0,0 +1,40 @@
+package com.bma.systemdesign.leaderelection;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class LeaderElectionTest {
+
+    @Test
+    void shouldConnectToZookeeper() {
+        var leaderElection = new LeaderElection();
+
+        leaderElection.connectToZookeeper();
+        leaderElection.close();
+
+        assertNotNull(leaderElection);
+    }
+
+    @SneakyThrows
+    @Test
+    void shouldVolunteerForElection() {
+        var node1 = new LeaderElection();
+        var node2 = new LeaderElection();
+        var node3 = new LeaderElection();
+
+        var t1 = new Thread(node1::connectToZookeeper);
+        var t2 = new Thread(node2::connectToZookeeper);
+        var t3 = new Thread(node3::connectToZookeeper);
+
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        Thread.sleep(5000);
+//        await().atLeast(Duration.ofMillis(10000));
+    }
+
+}
