dbf6c28ab620de960a119cac3ed425fe2002057f
vslala <varunshrivastava007@gmail.com>
2025-04-28 09:08:02 +0100

    citadel leetcode - wordbreak with dynamic programming

---
+++ src/main/java/com/bma/problemsolving/leetcode/java/citadel/WordBreak.java
@@ -0,0 +1,85 @@
+package com.bma.problemsolving.leetcode.java.citadel;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WordBreak {
+
+    private static class TrieNode {
+        int count;
+        String word;
+        Map<Character, TrieNode> trieNodes;
+
+        public TrieNode() {
+            this.count = 0;
+            this.word = "";
+            this.trieNodes = new HashMap<>();
+        }
+
+        public TrieNode get(char c) {
+            return this.trieNodes.get(c);
+        }
+
+        public void put(char c) {
+            if (!trieNodes.containsKey(c)) {
+                this.trieNodes.put(c, new TrieNode());
+            }
+        }
+
+        public void markWord(String word) {
+            this.word = word;
+            this.count += 1;
+        }
+    }
+
+    public boolean wordBreakWithTrie(String s, List<String> wordDict) {
+        TrieNode trie = buildTrie(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 0; i < s.length(); i++) {
+            if (!dp[i]) continue;
+            TrieNode node = trie;
+            for (int j = i; j < s.length(); j++) {
+                node = node.get(s.charAt(j));
+                if (node == null) break;
+                if (null != node.word && !node.word.isEmpty()) {
+                    dp[j + 1] = true;
+                }
+            }
+        }
+
+        return dp[s.length()];
+
+    }
+
+    private static TrieNode buildTrie(List<String> wordDict) {
+        var trie = new TrieNode();
+        for (String word : wordDict) {
+            TrieNode curr = trie;
+            for (char c: word.toCharArray()) {
+                curr.put(c);
+                curr = curr.get(c);
+            }
+            curr.markWord(word);
+        }
+
+        return trie;
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!dp[i]) continue;
+            for (int j = i; j <= s.length(); j++) {
+                if (wordDict.contains(s.substring(i, j))) {
+                    dp[j] = true;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
---
+++ src/test/java/com/bma/problemsolving/leetcode/java/citadel/WordBreakTest.java
@@ -0,0 +1,34 @@
+package com.bma.problemsolving.leetcode.java.citadel;
+
+import com.bma.problemsolving.leetcode.java.LeetCodeInputExpressionParser;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordBreakTest {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "leetcode [[leet,code]] true",
+            "applepenapple [[apple,pen]] true",
+            "catsandog [[cats,dog,sand,and,cat]] false",
+            "varunandpriyankaarerocking [[are,varun,rocking,priyanka]] false",
+            "anything [[]] false",
+            "aaaaaaa [[a,aa,aaa]] true",
+            "helloworld [[x,y,z]] false",
+            "cars [[car,ca,rs]] true",
+            "applepenapple [[apple,pen,applepen]] true",
+            "aaaaaaaaaaaaaaaaaaaaaaab [[a,aa,aaa,aaaa,aaaaa]] false",
+            "catsanddog [[cat,cats,and,sand,dog,random]] true",
+            "bb [[a,b,bbb,bbbb]] true"
+    }, delimiter = ' ')
+    void it_should_return_whether_the_string_can_be_spaced_into_the_dict_words_provided(String input, String expr, boolean expected) {
+        List<List<String>> dict = LeetCodeInputExpressionParser.parseNestedArrExpression(expr, ',', String.class);
+        var sol = new WordBreak();
+        boolean output = sol.wordBreakWithTrie(input, dict.getFirst());
+
+        assertEquals(expected, output);
+    }
+}
