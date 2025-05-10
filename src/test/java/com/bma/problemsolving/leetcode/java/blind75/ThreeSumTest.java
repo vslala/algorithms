4cb0cb5d421ee534e83a1ded02ce4d4310ab61f7
vslala <varunshrivastava007@gmail.com>
2025-05-10 16:34:56 +0100

    make classes package private

--- src/test/java/com/bma/problemsolving/leetcode/java/blind75/ThreeSumTest.java
+++ src/test/java/com/bma/problemsolving/leetcode/java/blind75/ThreeSumTest.java
@@ -15,12 +15,12 @@ class ThreeSumTest {
     @CsvSource(value = {
             "[[-1,0,1,2,-1,-4]] [[-1,-1,2],[-1,0,1]]",
             "[[0,0,0]] [[0,0,0]]",
-            "[[0,1,1]] [[]]",
+            "[[0,1,1]] _",
             "[[2,-3,0,-2,-5,-5,-4,1,2,-2,2,0,2,-4,5,5,-10]] [[-10,5,5],[-5,0,5],[-4,2,2],[-3,-2,5],[-3,1,2],[-2,0,2]]"
     }, delimiter = ' ')
     void it_should_return_all_triplets_whose_sum_is_zero(String inputExpr, String expectedExpr) {
         int[] input = convertToPrimitiveArrMatrix(parseNestedArrExpression(inputExpr, Integer.class))[0];
-        List<List<Integer>> expected = parseNestedArrExpression(expectedExpr, Integer.class);
+        List<List<Integer>> expected = expectedExpr.equals("_") ? List.of() : parseNestedArrExpression(expectedExpr, Integer.class);
 
         Util.println(input);
         Util.println(expected);
