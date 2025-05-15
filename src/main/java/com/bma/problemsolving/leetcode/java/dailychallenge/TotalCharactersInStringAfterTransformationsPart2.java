c3f774700c24f881041d250b2a8576ac87dd9142
vslala <varunshrivastava007@gmail.com>
2025-05-15 22:26:42 +0100

    learn matrix exponentiation and fast matrix multiplication then take a stab at this problem

---
+++ src/main/java/com/bma/problemsolving/leetcode/java/dailychallenge/TotalCharactersInStringAfterTransformationsPart2.java
@@ -0,0 +1,17 @@
+package com.bma.problemsolving.leetcode.java.dailychallenge;
+
+import java.util.List;
+
+class TotalCharactersInStringAfterTransformationsPart2 {
+    /**
+     * TODO: Learn and execute
+     * @param s
+     * @param t
+     * @param nums
+     * @return
+     */
+    public int lengthAfterTransformations(String s, int t, List<Integer> nums) {
+
+        return 0;
+    }
+}
---
+++ src/test/java/com/bma/problemsolving/leetcode/java/dailychallenge/TotalCharactersInStringAfterTransformationsPart2Test.java
@@ -0,0 +1,25 @@
+package com.bma.problemsolving.leetcode.java.dailychallenge;
+
+import com.bma.fixtures.Fixtures;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TotalCharactersInStringAfterTransformationsPart2Test {
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "abcyy 2 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2] 7"
+    }, delimiter = ' ')
+    void given_a_string_and_count_of_transformations_and_list_containing_the_number_of_next_characters_it_should_return_the_length_of_the_final_transformed_string(String s, int t, String numExpr, int expected) {
+        int[] nums = Fixtures.parse1DArray(numExpr);
+
+        var sol = new TotalCharactersInStringAfterTransformationsPart2();
+        int output = sol.lengthAfterTransformations(s, t, Arrays.stream(nums).boxed().toList());
+
+        assertEquals(expected, output);
+    }
+}
