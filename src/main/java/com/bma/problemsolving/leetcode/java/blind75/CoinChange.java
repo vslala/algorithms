c7307246501c5ef08a6a6efb74a9e492bf73c179
vslala <varunshrivastava007@gmail.com>
2025-05-17 09:32:10 +0100

    solve the coin change problem using dynamic programming

---
+++ src/main/java/com/bma/problemsolving/leetcode/java/blind75/CoinChange.java
@@ -0,0 +1,40 @@
+package com.bma.problemsolving.leetcode.java.blind75;
+
+import com.bma.algorithms.sort.elementary.Util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @author Varun Shrivastava
+ * @github www.github.com/vslala
+ * @date 16/05/2025
+ */
+class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int INF = amount + 1;
+        int[] nodes    = new int[amount + 1];
+        int[] prevCoin = new int[amount + 1];
+        Arrays.fill(nodes,    INF);
+        Arrays.fill(prevCoin, -1);
+        nodes[0] = 0;
+
+        for (int i = 1; i <= amount; i++) { // ← “visit” node i
+            for (int coin : coins) {        // ← explore each outgoing edge i->i–c
+                if (coin <= i && nodes[i - coin] + 1 < nodes[i]) {
+                    nodes[i]    = nodes[i - coin] + 1;
+                    prevCoin[i] = coin;     // remember that using coin c was best
+                }
+            }
+        }
+
+        if (nodes[amount] == INF) return -1;
+
+        var result = new ArrayList<Integer>();
+        for (int i = amount; i > 0; i -= prevCoin[i]) {
+            result.add(prevCoin[i]);
+        }
+
+        return result.size();
+    }
+}
---
+++ src/test/java/com/bma/problemsolving/leetcode/java/blind75/CoinChangeTest.java
@@ -0,0 +1,33 @@
+package com.bma.problemsolving.leetcode.java.blind75;
+
+import com.bma.fixtures.Fixtures;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Varun Shrivastava
+ * @github www.github.com/vslala
+ * @date 16/05/2025
+ */
+class CoinChangeTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "[1,2,5] 11 3",
+            "[1,2,5] 3 2",
+            "[2] 3 -1",
+            "[1,2,5] 7 2",
+            "[1] 0 0",
+            "[1] 1 1"
+    }, delimiter = ' ')
+    void shouldReturnTheMinCoinsRequiredToReachTheTarget(String coinsStr, int amount, int expected) {
+        int[] coins = Fixtures.parse1DArray(coinsStr);
+
+        var sol = new CoinChange();
+        int output = sol.coinChange(coins, amount);
+
+        assertEquals(expected, output);
+    }
+}
