1ec3827567306ee8ad4f679da67652eba882b736
vslala <varunshrivastava007@gmail.com>
2025-12-04 23:00:37 +0000

    [AdventOfCode2025] day 2 - part 2 - bruteforce matching patterns

--- src/main/java/com/bma/problemsolving/adventofcode2025/Day2GiftShop.java
+++ src/main/java/com/bma/problemsolving/adventofcode2025/Day2GiftShop.java
@@ -2,6 +2,8 @@ package com.bma.problemsolving.adventofcode2025;
 
 import com.bma.algorithms.sort.elementary.Util;
 
+import java.util.Set;
+
 /**
  * @author Varun Shrivastava
  * @github www.github.com/vslala
@@ -30,6 +32,33 @@ class Day2GiftShop {
             }
         }
 
+        return sum;
+    }
+
+    public long part2(String input) {
+        long sum = 0;
+        String[] ranges = input.split(",");
+        for (String range : ranges) {
+            String[] parts = range.split("-");
+            long start = Long.parseLong(parts[0]);
+            long end = Long.parseLong(parts[1]);
+            while (start <= end) {
+                String number = String.valueOf(start);
+                int numberLength = number.length();
+                for (int patternLength = 1; patternLength <= numberLength / 2; patternLength++) {
+                    // the repeated pattern length must be a divisor of the length
+                    if (numberLength % patternLength == 0) {
+                        boolean isRepeated = number.substring(0, patternLength)
+                                .repeat(numberLength / patternLength).equals(number);
+                        if (isRepeated) {
+                            sum += start;
+                            break;
+                        }
+                    }
+                }
+                start += 1;
+            }
+        }
         return sum;
     }
 }
